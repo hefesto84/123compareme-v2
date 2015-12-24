41a8bf3fc25642d49f3e19d792436be07737a05d
Roc Prat <r.prat@ubiqua.es>
2015-12-24 14:20:35 +0100

    HM

--- src/main/webapp/js/widgetHM.js
+++ src/main/webapp/js/widgetHM.js
@@ -58,8 +58,26 @@ var hotelswidget = new (function(window, document, jQuery){
     	setPriceInWidget : function(price,currency){
     		$('#widget123_top_bottom_price').find('#price').html(price.toFixed(2));
     		$('#widget123_top_bottom_price').find('#currency').html(currency);
+    		
+    		$('#widget123_responsive_left_price').find('.price').html(Math.floor(price));
+    		$('#widget123_responsive_left_price').find('.currency').html((price + "").split(".")[1]+currency);
+    		
+    		$('#widget123_popup_content_ota_right_price').html(price.toFixed(2));
+            $('#widget123_popup_content_ota_right_currency').html(currency);
     	},
     	setWidgetJavascript : function(){
+    		
+    		var widget_responsive = $('#widget123_responsive_div').html();
+    		$(widget_responsive).insertBefore('.wrap-frame');
+    		
+    		var widget_width = $('.wrap-cols').find('.col-text').width() + parseInt($('.wrap-cols').find('.col-text').css('padding-left')) + parseInt($('.wrap-cols').find('.col-text').css('padding-right'));
+    		var position_right = $('body').width() - ($('.wrap-cols').find('.col-text').offset().left + widget_width);
+    		var widget_top = $('.wrap-cols').find('.col-text').height() + parseInt($('.wrap-cols').find('.col-text').css('padding-top')) + parseInt($('.wrap-cols').find('.col-text').css('padding-bottom')) + $('.wrap-cols').find('.col-text').offset().top + 10;
+    		$('#widget123').css({
+    			'width' : widget_width,
+    			'right' : position_right,
+    			'top' : widget_top
+    		});
     		$('#widget123_top').click(function(){
                 if ($('#widget123_middle').is(':hidden')){
                     $('#widget123_middle').slideDown(1000);
@@ -67,15 +85,26 @@ var hotelswidget = new (function(window, document, jQuery){
                     $('#widget123_middle').slideUp(1000);
                 }
             });
+    		$('#widget123_responsive').click(function(){
+                $('#widget123_popup').show();
+                _paq.push(['trackEvent', 'Widget', 'Click', 'Widget clicado']);
+            });
+            $('#widget123_popup_content_top_close').click(function(){
+                $('#widget123_popup').hide();
+            });
     	},
     	setWidgetData : function(datos,price,currency,diffDay){
     		data = datos;
+            $('.widget123_content_loading').hide();
+            $('#widget123_popup_loading_text').hide();
             if(data.currency === 'XXX'){
             	_paq.push(['trackEvent', 'Widget', 'No currency', 'Currency no disponible']);
-                return 0;
+            	$('#widget123_popup_content_middle').append("<div id='no_otas'>Comparison not available in this currency</div>");
+            	return 0;
             }
             if (data.datos.length == 0){
                 _paq.push(['trackEvent', 'Widget', 'No results', 'No se ha mostrado el widget por que no hay datos']);
+                $('#widget123_popup_content_middle').append("<div id='no_otas'>Sorry, we couldn\'t complete the search for these dates</div>");
                 return 0;
             }
             var content_middle = document.getElementById("widget123_middle");
@@ -105,7 +134,34 @@ var hotelswidget = new (function(window, document, jQuery){
                     content_middle.appendChild(element);
                 }
             }
+            
+            var content_middle_popup = document.getElementById("widget123_popup_content_middle");
+            for (var j = 0; j < data.datos.length; j++) {
+
+                var precio_convertido = parseFloat(data.datos[j].price);
+
+                if (((Math.round(price) - 1) < Math.round(precio_convertido) || (Math.round(price) - 1) == Math.round(precio_convertido))){
+                    var element = document.createElement("div");
+                    element.setAttribute('id', 'element_popup');
+
+                    var image = document.createElement('img');
+                    image.setAttribute('src', domain + '/img/' + data.datos[j].site);
+                    element.appendChild(image);
+
+                    var span = document.createElement('span');
+                    span.setAttribute('class', 'priceWidgetElement');
+                    span.innerHTML = precio_convertido.toFixed(2) + ' ' + currency;
+                    element.appendChild(span);
+
+                    var clear = document.createElement('div');
+                    clear.setAttribute('style', 'clear:both;');
+                    element.appendChild(clear);
+
+                    content_middle_popup.appendChild(element);
+                }
+            }
             if (count === 0){
+            	$('#widget123_popup_content_middle').append("<div id='no_otas'>Sorry, we couldn\'t complete the search for these dates</div>");
                 _paq.push(['trackEvent', 'Widget', 'No results', 'No se han mostrado resultados']);
             } else {
             	_paq.push(['trackEvent', 'Widget', 'Show', 'Widget mostrado correctamente']);
