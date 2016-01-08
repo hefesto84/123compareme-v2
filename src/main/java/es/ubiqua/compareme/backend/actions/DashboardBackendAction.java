6fd0b5edfd5b6b8ec473312355ff0e7e1257c658
Dani Santeugini <dani@ubiqua.es>
2016-01-08 10:20:48 +0100

    pre-widget-piwik

--- src/main/java/es/ubiqua/compareme/backend/actions/DashboardBackendAction.java
+++ src/main/java/es/ubiqua/compareme/backend/actions/DashboardBackendAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.opensymphony.xwork2.ActionSupport;
 
 import es.ubiqua.compareme.manager.OtaManager;
+import es.ubiqua.compareme.model.Customer;
 import es.ubiqua.compareme.model.Hotel;
 import es.ubiqua.compareme.model.Ota;
 import es.ubiqua.compareme.utils.Utils;
@@ -16,13 +17,18 @@ public class DashboardBackendAction extends BaseBackendAction{
 	private List<Ota> otas;
 	private List<Hotel> hotels;
 	private OtaManager otaManager = new OtaManager();
-
+	private Customer customer;
+	
 	private int numOtas = 0;
 	
 	public String execute(){
 		
 		if(!isLogged()){ return ERROR; }
 		
+		customer = getLoggedCustomer();
+		if(customer.getId() == 10000){
+			customer.setId(1);
+		}
 		otas = otaManager.list();
 		setNumOtas(otas.size());
 		
@@ -59,6 +65,14 @@ public class DashboardBackendAction extends BaseBackendAction{
 
 	public void setNumOtas(int numOtas) {
 		this.numOtas = numOtas;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 
 }
